b8dc084de9dcb96b62a33e42bd142c89352664e2
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-24 22:32:19 +0500

    js

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@
 
 // console.log(multiples(3,5))
 
+//2-masala
+
+
+function oddOne(arr){
+    let count = 0;
+    for(let i = 0 ; i < arr.length ; i++){
+        if(arr[i] % 2 == 1){
+            count++
+            return i
+        }
+        
+    }
+    if(count == 0){
+        return -1
+    }
+}
+
+
+let arr = [2, 4, 6, 12, 10];//3
+
+console.log(oddOne(arr));
