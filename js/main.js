d675117d2210ea38b77791f7ec47c2a562056a05
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-25 06:18:34 +0500

    js

--- js/main.js
+++ js/main.js
@@ -14,21 +14,40 @@
 //2-masala
 
 
-function oddOne(arr){
-    let count = 0;
-    for(let i = 0 ; i < arr.length ; i++){
-        if(arr[i] % 2 == 1){
-            count++
-            return i
-        }
+// function oddOne(arr){
+
+//     let arr2 = arr.map(arr => Math.abs(arr))
+
+
+//     let count = 0;
+
+//     for(let i = 0 ; i < arr2.length ; i++){
+
         
-    }
-    if(count == 0){
-        return -1
-    }
-}
+//         if(arr2[i] % 2 == 1 ){
+            
+//             count++
+//             return i
+//         }
+        
+//     }
+//     if(count == 0){
+//         return -1
+//     }
+// }
 
 
-let arr = [2, 4, 6, 12, 10];//3
+// let arr = [-2, 4, 6, 7, 10];//3
+// let arr1 = [2, 16, 98, 10, 13, 78];
+// let arr2 = [4, -8, 98, -12, -7, 90, 100];
+// let arr3 = [2, 4, 6, 8];
 
-console.log(oddOne(arr));
+// console.log(oddOne(arr));
+// console.log(oddOne(arr1));
+// console.log(oddOne(arr2));
+// console.log(oddOne(arr3));
+
+// 3-masala
+
+
+
